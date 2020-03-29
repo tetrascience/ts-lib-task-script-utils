5ee5c7dae7a7303ad4d66486606edb75c00f8cb2
Kai Wang <wk.open@gmail.com>
2020-03-29 14:03:39 -0400

    Added "isnumber" util function

---
+++ utils/common/is_number.py
@@ -0,0 +1,24 @@
+import numpy as np
+from numbers import Number
+
+
+def isnumber(value):
+    """ Check if the target value is a number.
+    
+    Args:
+        value ():  The input under test
+    
+    Returns:
+        (bool): True if the target is a number. False otherwise.
+    """
+    try:
+        # Note boolean can be converted to number so it has to be avoided
+        if type(value) is bool:
+            return False
+
+        if isinstance(float(value), Number):
+            return True
+
+        return False
+    except:
+        return False
