0b83746ae97d2a21aabbd00f3edbdbbd86cbe9e6
Kai Wang <wk.open@gmail.com>
2020-08-09 16:25:58 -0400

    DE-1217: improved datetime conversion function

--- __test__/test_convert_datetime_to_ts_format.py
+++ __test__/test_convert_datetime_to_ts_format.py
@@ -1,6 +1,6 @@
 import pytest, sys
 from dateutil import tz
-from time_conversion import raw_time_string_to_ts_format
+from common.convert_datetime_to_ts_format import convert_datetime_to_ts_format
 
 
 def test_no_timezone():
@@ -9,7 +9,7 @@ def test_no_timezone():
         ("2020-06-18 13:17:57.54036", "2020-06-18T13:17:57.540"),
     ]
     for raw, expect in raw_expect:
-        assert raw_time_string_to_ts_format(raw) == expect
+        assert convert_datetime_to_ts_format(raw) == expect
 
 
 def test_embedded_timezone():
@@ -18,7 +18,7 @@ def test_embedded_timezone():
         ("2020-04-30T20:27:41.000Z", "2020-04-30T20:27:41.000Z"),
     ]
     for raw, expect in raw_expect:
-        assert raw_time_string_to_ts_format(raw) == expect
+        assert convert_datetime_to_ts_format(raw) == expect
 
 
 def test_input_timezone():
@@ -33,35 +33,33 @@ def test_input_timezone():
         ("2020-04-30T20:27:41.000Z", "GMT+3", "2020-04-30T17:27:41.000Z"),
     ]
     for raw, timezone, expect in raw_timezone_expect:
-        assert raw_time_string_to_ts_format(raw, from_zone=timezone) == expect
+        assert convert_datetime_to_ts_format(raw, timezone=timezone) == expect
 
 
 def test_input_timezone_overrides_embedded():
     raw_timezone_expect = [
         ("2020-03-06T17:19:45.706000-05:00", "GMT+5", "2020-03-06T12:19:45.706Z")
     ]
-    for raw, timezone, expect in raw_timezone_expect:
-        assert raw_time_string_to_ts_format(raw, from_zone=timezone) == expect
+    for raw, raw_timezone, expect in raw_timezone_expect:
+        assert convert_datetime_to_ts_format(raw, timezone=raw_timezone) == expect
 
 
 def test_unreadable_format():
     try:
-        raw_time_string_to_ts_format("20200512T235847.070Z")
+        convert_datetime_to_ts_format("20200512T235847.070Z")
     except:
         exc_info = sys.exc_info()
-        assert exc_info[
-            0
-        ] == ValueError and "Could not parse input datetime string" in str(exc_info[1])
+        assert exc_info[0] == ValueError
 
 
 def test_format_strings():
     raw_format_expect = [
         (
             "2020-03-06T17:19:45.706000-05:00",
-            ["YYYY-MM-DDTHH:mm:ss.SZZ"],
+            "YYYY-MM-DDTHH:mm:ss.SZZ",
             "2020-06-03T22:19:45.706Z",
         ),
-        ("20200512T235847.070Z", ["YYYYMMDDTHHmmss.SZ"], "2020-05-12T23:58:47.070Z"),
+        ("20200512T235847.070Z", "YYYYMMDDTHHmmss.SZ", "2020-05-12T23:58:47.070Z"),
     ]
 
 
@@ -69,18 +67,18 @@ def test_YDM_order():
     raw_format_expect = [
         (
             "2020-03-06T17:19:45.706000-05:00",
-            ["YYYY-DD-MMTHH:mm:ss.SZZ"],
+            "YYYY-DD-MMTHH:mm:ss.SZZ",
             "2020-06-03T22:19:45.706Z",
         )
     ]
-    for raw, formats, expect in raw_format_expect:
-        assert raw_time_string_to_ts_format(raw, datetime_formats=formats) == expect
+    for raw, format, expect in raw_format_expect:
+        assert convert_datetime_to_ts_format(raw, format) == expect
 
 
 def test_bad_timezone():
     try:
-        raw_time_string_to_ts_format(
-            "2020-03-06T17:19:45.706000-05:00", from_zone="GMT+500"
+        convert_datetime_to_ts_format(
+            "2020-03-06T17:19:45.706000-05:00", timezone="GMT+500"
         )
     except:
         exc_info = sys.exc_info()
