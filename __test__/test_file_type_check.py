37188ffd7d7b2631aa8b864b427d7ddcac7afb0e
Tal Aharon <taharon@tetrascience.com>
2020-08-12 16:40:30 -0400

    #DE-1372 pytest now catches the test failing for test4

--- __test__/test_file_type_check.py
+++ __test__/test_file_type_check.py
@@ -5,4 +5,5 @@ def test_file_type_check():
   file_type_check('/tetrasicence/123456/example-file1.txt', 'txt')
   file_type_check('example-file1.txt', 'txt')
   file_type_check('/tetrasicence/123456/example-file1.xlsx', ['txt', 'xlsx', 'csv'])
-  file_type_check('/tetrasicence/123456/example-file1.txt', ['xlsx', 'csv'])
+  with pytest.raises(SystemExit):
+    file_type_check('/tetrasicence/123456/example-file1.txt', ['xlsx', 'csv'])
