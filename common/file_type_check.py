dee695590f2d3cedef017a4f1106f20c03089995
Tal Aharon <taharon@tetrascience.com>
2020-08-12 15:26:20 -0400

    #DE-1372 added string reversal of extension after getting it

--- common/file_type_check.py
+++ common/file_type_check.py
@@ -1,4 +1,5 @@
 import typing as t
+import sys
 
 def file_type_check(filename: str, expected_type: t.Union[str, list]):
   """
@@ -9,7 +10,7 @@ def file_type_check(filename: str, expected_type: t.Union[str, list]):
    expected_type can be a string or array 
   """
   # get the extension from filename
-  extension = filename[::-1].strip().split('.')[0].lower()
+  extension = filename[::-1].strip().split('.')[0].lower()[::-1]
   try:
     if type(expected_type) == str:
       # lower case both the extension and expected_type, do a string match
@@ -17,10 +18,10 @@ def file_type_check(filename: str, expected_type: t.Union[str, list]):
     elif type(expected_type) == list:
       assert extension in [file_type.lower() for file_type in expected_type]
   except AssertionError:
-    print(f"The pipeline is expecting the file type to be {expected_type}, but the provided file has a file type of {extension}")
+    sys.exit(f"The pipeline is expecting the file type to be {expected_type}, but the provided file has a file type of {extension}.")
 
 if __name__ == "__main__":
   file_type_check('/tetrasicence/123456/example-file1.txt', 'txt')
   file_type_check('example-file1.txt', 'txt')
-  file_type_check('/tetrasicence/123456/example-file1.txt', ['txt', 'xlsx', 'csv'])
+  file_type_check('/tetrasicence/123456/example-file1.xlsx', ['txt', 'xlsx', 'csv'])
   file_type_check('/tetrasicence/123456/example-file1.txt', ['xlsx', 'csv'])
