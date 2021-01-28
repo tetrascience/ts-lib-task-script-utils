9c0dbc2a2b756b04d1a399342a196f5ad676b619
Kai Wang <kwang@tetrascience.com>
2021-01-27 22:16:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,13 +19,15 @@ setup(
     name=NAME,
     version=VERSION,
     description="Python utility functions to help write efficient Tetra Task Scripts",
-    author="TetraScience",
-    author_email="",
-    url="",
+    author="tetrascience",
+    author_email="developers@tetrascience.com",
+    url="https://developers.tetrascience.com",
     keywords=[],
     install_requires=requirements,
     packages=find_packages(),
     include_package_data=True,
     long_description=readme,
-    long_description_content_type="text/markdown"
+    long_description_content_type="text/markdown",
+    python_requires='>=3.7',
+    license='Apache License 2.0'
 )
